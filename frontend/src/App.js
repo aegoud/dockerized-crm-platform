78f7e7fdd81dc6e46a1b4a6cfad6517b7aaab56e
Anil Ediga <anil.devops12@gmail.com>
2026-06-03 13:50:52 -0400

    Initial CRM Docker Project

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div style={{textAlign:'center', marginTop:'100px'}}>
+      <h1>Customer Relationship Management (CRM) Platform</h1>
+
+      <h3>Containerized with Docker & Docker Compose</h3>
+
+      <p>Frontend: React</p>
+      <p>Backend: Node.js</p>
+      <p>Database: MongoDB</p>
+
+      <h4>Built by Anil Ediga</h4>
+    </div>
+  );
+}
+
+export default App;
